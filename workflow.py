491c758441828dfe2e05fade9fb4e95c4cf25a3b
Philip N <phi.nguyen@laverne.edu>
2020-09-22 15:45:46 -0700

    Add db.json and json read code

--- workflow.py
+++ workflow.py
@@ -4,6 +4,16 @@ import time
 import webbrowser
 import subprocess
 
+import json
+
+with open('db.json', 'r+') as f:
+    data = json.load(f)
+    data["id"] = 999 # <--- add `id` value.
+    f.seek(0)        # <--- should reset file position to the beginning.
+    json.dump(data, f, indent=4)
+    f.truncate()     # remove remaining part
+
+
 def maxWindow():
     window = gw.getActiveWindow()
     window.maximize()
