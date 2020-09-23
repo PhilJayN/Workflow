d29cfd39c82f8bcf3db2099e305ee002528322b4
Philip N <phi.nguyen@laverne.edu>
2020-09-23 16:56:54 -0700

    Clean up comments

--- workflow.py
+++ workflow.py
@@ -6,12 +6,17 @@ import subprocess
 import json
 import PySimpleGUI as sg
 
+########################### Load / Save Settings ###########################
+def loadSettings():
+    with open('db.json', 'r') as f:
+        data = json.load(f)
+        # Return db.json to use in other fxns
+    return data
+
 def saveSettings(dictValues):
     # print('you called saveSettings with dictValues', dictValues["web_sites"])
     #dictValues looks like: {'web_sites': 'a', 'web_sites0': 'b'}, access using dictValues['web_sites0'], NOT index num
-    print('dictValues full:', dictValues)
     print('dictValues accessing w/ key set in PySimpleGUI layout code:', dictValues['web_sites0'])
-
     with open('db.json', 'r+') as f:
         data = json.load(f)
         # print(data["people"][0]["name"])
@@ -23,11 +28,7 @@ def saveSettings(dictValues):
             f.truncate()     # remove remaining part
     # sg.popup('Saved!')
 
-def loadSettings():
-    with open('db.json', 'r') as f:
-        data = json.load(f)
-    return data
-
+########################### GUI ###########################
 def startGUI():
     sg.theme('DarkAmber')
     layout = [  [sg.Text('Your Sites')],
@@ -37,19 +38,15 @@ def startGUI():
     [sg.Button('Save'), sg.Button('Cancel')] ]
 
     window = sg.Window('Workflow', layout, finalize=True)
-
     print('loadSettings return val is the json db:', loadSettings())
     db = loadSettings()
     # read that json db, and use window.update on where you want the data to render
     for index in range(0,2):
         window['web_sites' + str(index)].update(db["sites"][index]["url"])
-
-
     # Event Loop, gets values of inputs
     while True:
         event, values = window.read()
         print('even loop value dict:', values)
-        # print('data stuff:', type(values), values, 'len:', len(values))
         print('values:', values)
 
         saveSettings(values)
@@ -59,8 +56,8 @@ def startGUI():
             window.close()
             # db only updates when user closes the GUI??
 
-startGUI()
 
+########################### TEMP FXN ###########################
 def maxWindow():
     window = gw.getActiveWindow()
     window.maximize()
@@ -86,7 +83,6 @@ def exitPrompt():
         print('Exiting')
         time.sleep(.5)
 
-
 requestedSites = ['https://www.udemy.com/course/automate/learn/lecture/3465864#questions/11019006',
                 'https://automatetheboringstuff.com/2e/'
                 ]
@@ -100,14 +96,12 @@ def openSites():
         time.sleep(1)
     closeTabs()
 
-
 requestedFolders = ['D:\\tcg', 'C:\\Users\\asus270', 'C:\\Dropbox\\~Programming\\projects']
 
 def openFolders(folders):
     for i in range(len(requestedFolders)):
         subprocess.Popen(r'explorer ' + requestedFolders[i])
         print('req. folders:', requestedFolders[i])
-
 
 def closePrograms():
     subprocess.call([r'C:\Program Files\Mozilla Firefox\\firefox.exe'])
@@ -122,28 +116,15 @@ def closePrograms():
         print('Exiting')
         time.sleep(.5)
 
-
-
-def click(btnPos):
-    loginBtn = [1707, 682]
-    wpBtn = [2700, 248]
-
-    pyautogui.click(btnPos[0], btnPos[1], duration=.3)
-
-# time.sleep(2)
-# click(loginBtn)
-# time.sleep(25)
-# click(wpBtn)
-
-
-# TEMPORARY FUNCTION CALLERS
+# TEMPORARY FUNCTION CALLERS 123
 def functionHandlers():
     openSites()
     openFolders(requestedFolders)
 
-loadSettings()
-
 # functionHandlers()
+
+startGUI()
+loadSettings()
 
 # Cross platform open folders
 # def openDirXPlatform():
